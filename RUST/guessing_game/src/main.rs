2265059ff5ef82df91fa143a2c144be77e7b6110
Vishwajeet Bharadia <vishwajeet@CIPH3R>
2025-02-02 23:41:00 +0530

    number guessing game in RUST

--- RUST/guessing_game/src/main.rs
+++ RUST/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -10,7 +11,16 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("failed");
+
+    let guess: u32 = guess.trim().parse().expect("please type a number");
+
     println!("the secret number is {secret_number}");
 
-    println!("you guessed: {}", guess);
+    println!("you guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => print!("Too Large"),
+        Ordering::Equal => println!("you Win!!"),
+    }
 }
